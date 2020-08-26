440f611415e0fd5dcb9298993d71728183434d0b
Alejandro Soto <alesoto9113@gmail.com>
2020-08-25 20:53:23 -0400

    Test

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
-import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:flutter_compass/flutter_compass.dart';
+import 'package:geolocator/geolocator.dart';
 
 void main() => runApp(MyApp());
 
@@ -24,23 +24,82 @@ class MapSampleState extends State<MapSample> {
   double _direction;
   BitmapDescriptor carIcon;
   Set<Marker> markers = Set();
+  Geolocator _geolocator;
+  Position _position;
+  int x = 0;
+  LatLng location;
 
   static final CameraPosition _initialCameraPosition = CameraPosition(
     target: LatLng(-33.4479439, -70.598717),
     zoom: 16.4746,
   );
 
+  void checkPermission() {
+    _geolocator.checkGeolocationPermissionStatus().then((status) {
+      print('status: $status');
+    });
+    _geolocator
+        .checkGeolocationPermissionStatus(
+            locationPermission: GeolocationPermission.locationAlways)
+        .then((status) {
+      print('always status: $status');
+    });
+    _geolocator.checkGeolocationPermissionStatus(
+        locationPermission: GeolocationPermission.locationWhenInUse)
+      ..then((status) {
+        print('whenInUse status: $status');
+      });
+  }
+
+  void updateLocation() async {
+    try {
+      Position newPosition = await Geolocator()
+          .getCurrentPosition(desiredAccuracy: LocationAccuracy.high)
+          .timeout(new Duration(seconds: 5));
+      setState(() {
+        _position = newPosition;
+        if (_position != null) {
+          location = new LatLng(_position.latitude, _position.longitude);
+        }
+      });
+    } catch (e) {
+      print('Error: ${e.toString()}');
+    }
+  }
+
   @override
   void initState() {
     super.initState();
     BitmapDescriptor.fromAssetImage(
-            ImageConfiguration(platform: TargetPlatform.android), "assets/car.png")
+            ImageConfiguration(platform: TargetPlatform.android),
+            "assets/car.png")
         .then((onValue) {
       carIcon = onValue;
     });
     FlutterCompass.events.listen((double direction) {
       setState(() {
         _direction = direction;
+      });
+    });
+      location = new LatLng(0, 0);
+
+    _geolocator = Geolocator();
+    LocationOptions locationOptions = LocationOptions(
+        accuracy: LocationAccuracy.high, distanceFilter: 1, timeInterval: 500);
+
+    checkPermission();
+    updateLocation();
+
+
+    _geolocator.getPositionStream(locationOptions).listen((Position position) {
+      _position = position;
+      print("asdasdsadasdsadsad********************");
+      setState(() {
+        _position = position;
+        if (_position != null) {
+          location = new LatLng(_position.latitude, _position.longitude);
+          x = x + 1;
+        }
       });
     });
   }
@@ -50,16 +109,24 @@ class MapSampleState extends State<MapSample> {
     markers.addAll([
       Marker(
           markerId: MarkerId('value'),
-          position: LatLng(-33.4479439, -70.598717),
+          position: location,
           icon: carIcon,
           rotation: _direction),
     ]);
     return new Scaffold(
-      body: GoogleMap(
-        mapType: MapType.normal,
-        zoomControlsEnabled: false,
-        initialCameraPosition: _initialCameraPosition,
-        markers: markers
+      body: Stack(
+        children: <Widget>[
+          GoogleMap(
+              mapType: MapType.normal,
+              zoomControlsEnabled: false,
+              initialCameraPosition: _initialCameraPosition,
+              markers: markers),
+          Center(
+            child: Text(
+                'Latitude: ${_position != null ? _position.latitude.toString() : '0'},'
+                ' Longitude: ${_position != null ? _position.longitude.toString() : '0'} x: ${x.toString()}'),
+          )
+        ],
       ),
     );
   }
